0ca0f104ecd894fd2c3184c4e7570a9e5cd66574
晓森 <1769811680@qq.com>
2020-12-22 15:31:12 +0800

    phoneModels

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
-  }
-]
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: '/',
+      component: () => import('../App.vue'),
+      children: [
+        {
+          path: '/',
+          component: () => import('../views/home/index.vue')
+        }
+      ]
+    }
+  ]
 })
 
 export default router
---
+++ src/views/home/index.vue
@@ -0,0 +1,82 @@
+<template>
+  <van-cell-group>
+    <van-cell
+      v-for="phone of state.phones"
+      :key="phone.code"
+      is-link
+      @click="showPopup"
+      >{{ phone.name }}</van-cell
+    >
+  </van-cell-group>
+  <van-popup
+    position="bottom"
+    round
+    :style="{ height: '90%' }"
+    v-model:show="show"
+  >
+    <div class="bar"></div>
+  </van-popup>
+</template>
+
+<script>
+import { Cell, Popup, CellGroup } from "vant";
+import { reactive, ref } from "vue";
+
+export default {
+  name: "Home",
+  components: {
+    VanCell: Cell,
+    VanPopup: Popup,
+    VanCellGroup: CellGroup,
+  },
+  setup() {
+    const state = reactive({
+      phones: [
+        {
+          name: "华为",
+          code: "huawei",
+        },
+        {
+          name: "小米",
+          code: "xiaomi",
+        },
+        {
+          name: "OPPO",
+          code: "oppo",
+        },
+        {
+          name: "vivo",
+          code: "vivo",
+        },
+        {
+          name: "锤子",
+          code: "smartisan",
+        },
+        {
+          name: "魅族",
+          code: "meizu",
+        },
+      ],
+    });
+    const show = ref(false);
+    const showPopup = () => {
+      show.value = true;
+    };
+    return {
+      show,
+      showPopup,
+      state,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.bar {
+  height: 6px;
+  width: 50px;
+  background-color: #ccc;
+  border-radius: 16px;
+  margin: 5px auto;
+}
+</style>
